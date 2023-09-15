559ef211d42e509d9fa38ee3ef2105dc83553171
anjaneyulu19se <144314605+anjaneyulu19se@users.noreply.github.com>
2023-09-15 11:15:14 +0530

    first java programe on eclipse

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,9 @@
-class Welcome{
-	public static void main(String[] args){
-	System.out.println("Welcome to Java Programme");
+package com.core.JavaPractise;
 
-}
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my First java programme");
+	}
+	
 }
